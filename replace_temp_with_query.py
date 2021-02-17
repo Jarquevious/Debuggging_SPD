4d4ae2fa46a702d0bc3d246be1cf2de796bc1b14
Jarquevious <jarquevious@gmail.com>
2021-02-17 14:40:13 -0800

    replace temp with var complete

--- replace_temp_with_query.py
+++ replace_temp_with_query.py
@@ -20,6 +20,7 @@ class Student:
 class School:
     def __init__(self, students) -> None:
         self.students = students
+
     def process_graduation(self):
         # Find the students in the school who have successfully graduated.
         min_gpa = 2.5 # minimum acceptable GPA
@@ -27,15 +28,28 @@ class School:
         for s in self.students:
             if s.get_gpa() > min_gpa:
                 passed_students.append(s)
+        return passed_students
 
+    def print_graduate_name(self):            
         # print student's name who graduated.
+        ps= self.process_graduation()
         print('*** Student who graduated *** ')
-        for s in passed_students:
+        for s in ps:
             print(s.name)
         print('****************************')
+
+    def send_email(self):
+        self.process_graduation()
+        self.print_graduate_name()
         # Send congrat emails to them.
         for s in passed_students:
-            s.send_congrat_email()
+            print(s.send_congrat_email())
+            
+
+    def find_top_10(self):
+        self.process_graduation()
+        self.print_graduate_name()
+        self.send_email()
         # Find the top 10% of them and send their contact to the top employers
         passed_students.sort(key=lambda s: s.get_gpa())
         percentile = 0.9
@@ -43,9 +57,10 @@ class School:
         top_10_percent = passed_students[index:]
         top_employers = [Employer('Microsoft'), Employer('Free Software Foundation'), Employer('Google')]
         for e in top_employers:
-            e.send(top_10_percent)
+            print(e.send(top_10_percent))
 
 students = [Student(2.1, 'donald'), Student(2.3, 'william'), Student(2.7, 'toro'), 
             Student(3.9, 'lili'), Student(3.2,'kami'), Student(3,'sarah')]
 school  = School(students)
 school.process_graduation()
+school.print_graduate_name()
