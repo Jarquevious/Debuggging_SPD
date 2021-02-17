ceecc5952d845dd0707d2f250ee1a576b5338971
Jarquevious <jarquevious@gmail.com>
2021-02-17 14:03:24 -0800

    introduce explaining variable3 works

--- introduce_explaining_variable3.py
+++ introduce_explaining_variable3.py
@@ -5,8 +5,14 @@ MEDIUM = 600000
 COOKED_CONSTANT = 0.05
 
 def is_cookeding_criteria_satisfied(time, temperature, pressure, desired_state):
-    if desired_state == 'well-done' and time * temperature * pressure * COOKED_CONSTANT >= WELL_DONE: 
+    technique = time * temperature * pressure * COOKED_CONSTANT
+    if desired_state == 'well-done' and technique >= WELL_DONE:
+        print('Well Done')
         return True
-    if desired_state == 'medium' and time * temperature * pressure * COOKED_CONSTANT >= MEDIUM:
+    if desired_state == 'medium' and technique >= MEDIUM:
+        print('medium')
         return True
     return False
+
+well_done = 'well-done'
+is_cookeding_criteria_satisfied(10,900000,5,well_done)
